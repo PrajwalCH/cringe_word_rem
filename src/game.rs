3765b3874d152c194995ca646b81dbf54d40ec83
PrajwalCH <prajjwal2058@gmail.com>
2021-09-23 15:51:45 +0545

    new_feat: implement words vec idx map shuffler and random num generator

--- src/game.rs
+++ src/game.rs
@@ -39,3 +39,23 @@ fn read_words_file() -> Option<Vec<String>> {
 
     Some(words_vec)
 }
+
+fn shuffle_words_vec_idx_map(words_vec_idx_map: &mut Vec<usize>) {
+    assert_eq!(words_vec_idx_map.len(), words_vec_idx_map.capacity());
+
+    for i in (0..words_vec_idx_map.len()).rev() {
+        let mut random_index = generate_random_num(words_vec_idx_map.len());
+
+        if random_index == i {
+            random_index += 1 % words_vec_idx_map.len();
+        }
+
+        let last_temp = words_vec_idx_map[i];
+        words_vec_idx_map[i] = words_vec_idx_map[random_index];
+        words_vec_idx_map[random_index] = last_temp;
+    }
+}
+
+fn generate_random_num(max_range: usize) -> usize {
+    thread_rng().gen_range(0..max_range)
+}
