096df9894c66548a59350d6a7c4410a97bcf94c5
PrajwalCH <prajjwal2058@gmail.com>
2021-09-25 14:12:27 +0545

    refactor: extract most of the mode to lib.rs from main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,4 @@
-mod game;
-
-use std::io::{self, Write};
-
 fn main() {
-    loop {
-        print_menu();
-        let user_choice = take_user_choice();
-
-        match user_choice {
-            1 => game::start(),
-            2 => break,
-            _ => {
-                println!("Invalid menu option");
-                continue
-            },
-        }
-    }
+    cringe_word_rem::start();
 }
 
-fn print_menu() {
-    println!("1. Start");
-    println!("2. Exit");
-}
-
-fn take_user_choice() -> u8 {
-    print!("> ");
-    io::stdout().flush().unwrap();
-
-    let mut user_choice = String::new();
-
-    io::stdin()
-        .read_line(&mut user_choice)
-        .expect("Failed to read input");
-
-    let user_choice: u8 = match user_choice.trim().parse() {
-        Ok(num) => num,
-        Err(err) => {
-            println!("Oops!! only numbers are allowed: {:#?}", err);
-            0
-        }
-    };
-
-    user_choice
-}
